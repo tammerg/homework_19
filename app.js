d94dd0fd812a105894ec31b8469d367207032734
tammerg <tammergalal20@gmail.com>
2016-03-19 01:18:57 -0400

    added the beginning of angular functionality and can now add items to a table that will be listed for sale. Next to attatch mongo DB, then begin building out the MVC structure

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var yardSale = angular.module('yardApp', []);
 
 
-yardSale.controller('mainController', function(){
+yardSale.controller('mainController', function($scope){
+
+  $scope.products = [];
+
+  $scope.addProduct = function(){
+    $scope.products.push($scope.item);
+    $scope.item = {};
+  }
+
 });
