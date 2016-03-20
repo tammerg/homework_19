c8e291bb32ce8fdc86e2d1006d6ee8edd0fb9f1b
tammerg <tammergalal20@gmail.com>
2016-03-20 00:38:59 -0400

    reorganized to MVC structurer

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ var PORT = process.env.PORT || 8080;
 var db = 'mongodb://localhost/yardsale';
 mongoose.connect(db);
 
+app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.send(index.html);
+});
+
 app.listen(PORT, function(req, res){
-  console.log("You're listening on port %s:", PORT);
+  console.log("You're listening on port: %s", PORT);
 });
